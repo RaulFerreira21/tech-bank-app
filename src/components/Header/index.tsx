9e2ebf2385caad2c1e78f6e250f5006a8bf684ce
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2025-05-18 20:53:45 -0300

    feat: desenvolvimento da tela de transferencia

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,19 +1,19 @@
-"use client"
+"use client";
 
-import { Box, Typography } from "@mui/material"
-import AccountCircleIcon from "@mui/icons-material/AccountCircle"
-import LogoutIcon from "@mui/icons-material/Logout"
-import Link from "next/link"
+import { Box, Typography } from "@mui/material";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import LogoutIcon from "@mui/icons-material/Logout";
+import Link from "next/link";
 
 export default function HeaderComponent() {
-  const user: string = "userX"
+  const user: string = "userX";
 
   return (
     <Box
       component="header"
       sx={{
         bgcolor: "#236B7A",
-        backgroundImage: 'linear-gradient(45deg, #12120F, transparent);',
+        backgroundImage: "linear-gradient(45deg, #12120F, transparent);",
         color: "#FAFAFA",
         width: "100%",
         height: "56px",
@@ -48,10 +48,10 @@ export default function HeaderComponent() {
         <Link className="navigationLink" href={"/extrato"}>
           <Typography variant="body1">Extrato</Typography>
         </Link>
-         <Link className="navigationLink" href={"/deposito"}>
+        <Link className="navigationLink" href={"/deposito"}>
           <Typography variant="body1">Depósito</Typography>
         </Link>
-        <Link className="navigationLink" href={"/dashboard"}>
+        <Link className="navigationLink" href={"/transferir"}>
           <Typography variant="body1">Transferir</Typography>
         </Link>
       </Box>
@@ -59,5 +59,5 @@ export default function HeaderComponent() {
         <LogoutIcon></LogoutIcon>
       </Box>
     </Box>
-  )
+  );
 }
---
+++ src/pages/transferir.tsx
@@ -0,0 +1,80 @@
+import Title from "@/components/Title";
+import { Box, TextField, Typography } from "@mui/material";
+import Button from "@/components/Button";
+import { useState } from "react";
+
+export default function Transferir() {
+  const [contaDeposito, setContaDeposito] = useState("");
+
+  function handleConta(conta: string) {
+    setContaDeposito(conta);
+  }
+
+  return (
+    <>
+      <Title title="Realizar tranferência" />
+
+      <Box sx={{ p: 3 }}>
+        <Typography variant="subtitle1" sx={{ mb: 1, fontWeight: "bold" }}>
+          {"Para quem deseja transferir?"}
+        </Typography>
+        <TextField
+          fullWidth
+          variant="standard"
+          placeholder=""
+          slotProps={{
+            input: {
+              disableUnderline: false,
+            },
+          }}
+          sx={{ mb: 4 }}
+        />
+
+        <Typography variant="subtitle1" sx={{ mb: 1, fontWeight: "bold" }}>
+          {"Qual valor você deseja depositar?"}
+        </Typography>
+        <TextField
+          fullWidth
+          variant="standard"
+          placeholder="R$"
+          slotProps={{
+            input: {
+              disableUnderline: false,
+            },
+          }}
+          sx={{ mb: 4 }}
+        />
+
+        <Box sx={{ mb: 4, width: "100%" }}>
+          <Typography sx={{ mb: 1, fontWeight: "bold" }}>
+            De qual conta vai sair esse valor?
+          </Typography>
+
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              gap: 2,
+              justifyContent: "center",
+            }}
+          >
+            <Button
+              color={contaDeposito === "conta-corrente" ? "info" : "inherit"}
+              text="conta-corrente"
+              onClick={() => handleConta("conta-corrente")}
+            />
+            <Button
+              color={contaDeposito === "conta-poupança" ? "info" : "inherit"}
+              text="conta-poupança"
+              onClick={() => handleConta("conta-poupança")}
+            />
+          </Box>
+        </Box>
+
+        <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
+          <Button color="primary" text="Concluir" />
+        </Box>
+      </Box>
+    </>
+  );
+}
