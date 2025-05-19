7c3d75ad876a987f8531d140ab992f0188c071fb
Jackson dos Santos <jackson1santos@proton.me>
2025-05-18 21:15:28 -0300

    fix: corrige transicao de telas para home

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,27 @@
 "use client"
 
-import '@/app/globals.css'
-import MainContainer from '@/components/Container'
-import { useTheme } from '@/styles/useTheme'
-import { CssBaseline, ThemeProvider } from '@mui/material'
-import type { AppProps } from 'next/app'
+import "@/app/globals.css"
+import MainContainer from "@/components/Container"
+import { useTheme } from "@/styles/useTheme"
+import { CssBaseline, ThemeProvider } from "@mui/material"
+import type { AppProps } from "next/app"
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps, router }: AppProps) {
   const theme = useTheme()
+
+  if (router.pathname === "/") {
+    return (
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    )
+  }
 
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
+
       <MainContainer>
         <Component {...pageProps} />
       </MainContainer>
